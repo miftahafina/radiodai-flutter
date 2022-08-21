95c4760b7066ce70b4287c5701f37152c3f7194d
Miftah Afina <miftah.afina@gmail.com>
2022-08-21 14:58:53 +0700

    Add radio dai logo on about screen

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.teal
       ),
-      home: const AboutScreen(),
+      home: const PlayerScreen(),
     );
   }
 }
@@ -31,15 +31,30 @@ class AboutScreen extends StatelessWidget {
       appBar: AppBar(
         backgroundColor: Colors.transparent,
         elevation: 0,
+        leading: const BackButton(
+          color: Colors.grey,
+        ),
       ),
 
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 32),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: const [
-            Text(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(12),
+              child: Image.asset(
+                "images/logo_radio_dai.jpg",
+                fit: BoxFit.cover,
+                height: 80,
+                width: 80,
+              ),
+            ),
+            const SizedBox(
+              height: 18,
+            ),
+            const Text(
               "Tentang",
               textAlign: TextAlign.center,
               style: TextStyle(
@@ -47,20 +62,20 @@ class AboutScreen extends StatelessWidget {
                   fontWeight: FontWeight.bold
               ),
             ),
-            SizedBox(
+            const SizedBox(
               height: 18,
             ),
-            Text(
+            const Text(
               "Radio Da'i merupakan evolusi dari sebuah nama, yakni Nida'ul Irsyad, yang berarti seruan yang membimbing.",
               textAlign: TextAlign.center,
               style: TextStyle(
                 fontSize: 16
               ),
             ),
-            SizedBox(
+            const SizedBox(
               height: 18,
             ),
-            Text(
+            const Text(
               "Radio ini berdiri di bawah naungan Pesantren Islam Al-Irsyad Tengaran.",
               textAlign: TextAlign.center,
               style: TextStyle(
@@ -87,7 +102,11 @@ class PlayerScreen extends StatelessWidget {
         // title: const Text("Radio Da'i"),
         actions: [
           IconButton(
-            onPressed: () {},
+            onPressed: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context) {
+                return const AboutScreen();
+              }));
+            },
             icon: const Icon(
               Icons.info_outline_rounded,
               color: Colors.grey,
