e99bd57e3c9500163933312d9e1fc09ec7f7ccc9
Miftah Afina <miftah.afina@gmail.com>
2022-08-22 07:44:15 +0700

    Fix bottom overflow with SingleChildScrollView and CustomScrollView

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -36,53 +38,55 @@ class AboutScreen extends StatelessWidget {
         ),
       ),
 
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 32),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            ClipRRect(
-              borderRadius: BorderRadius.circular(12),
-              child: Image.asset(
-                "images/logo_radio_dai.jpg",
-                fit: BoxFit.cover,
-                height: 80,
-                width: 80,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 32),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(12),
+                child: Image.asset(
+                  "images/logo_radio_dai.jpg",
+                  fit: BoxFit.cover,
+                  height: 80,
+                  width: 80,
+                ),
               ),
-            ),
-            const SizedBox(
-              height: 18,
-            ),
-            const Text(
-              "Tentang",
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                  fontSize: 32,
-                  fontWeight: FontWeight.bold
+              const SizedBox(
+                height: 18,
               ),
-            ),
-            const SizedBox(
-              height: 18,
-            ),
-            const Text(
-              "Radio Da'i merupakan evolusi dari sebuah nama, yakni Nida'ul Irsyad, yang berarti seruan yang membimbing.",
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 16
+              const Text(
+                "Tentang",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    fontSize: 32,
+                    fontWeight: FontWeight.bold
+                ),
               ),
-            ),
-            const SizedBox(
-              height: 18,
-            ),
-            const Text(
-              "Radio ini berdiri di bawah naungan Pesantren Islam Al-Irsyad Tengaran.",
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                  fontSize: 16
+              const SizedBox(
+                height: 18,
               ),
-            )
-          ],
+              const Text(
+                "Radio Da'i merupakan evolusi dari sebuah nama, yakni Nida'ul Irsyad, yang berarti seruan yang membimbing.",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    fontSize: 16
+                ),
+              ),
+              const SizedBox(
+                height: 18,
+              ),
+              const Text(
+                "Radio ini berdiri di bawah naungan Pesantren Islam Al-Irsyad Tengaran.",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    fontSize: 16
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
@@ -115,76 +119,83 @@ class PlayerScreen extends StatelessWidget {
         ],
       ),
 
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              ClipRRect(
-                borderRadius: BorderRadius.circular(12),
-                child: Image.asset(
-                  "images/album_art.jpg",
-                  fit: BoxFit.cover,
-                  height: 300,
-                  width: 300,
-                ),
-              )
-            ],
-          ),
-
-
-          Expanded(
+      body: CustomScrollView(
+        slivers: [
+          SliverFillRemaining(
+            hasScrollBody: false,
             child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
-                Column(
-                  children: const [
-                    Text(
-                      "Radio Da'i",
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                          fontSize: 32,
-                          fontWeight: FontWeight.bold
-                      ),
-                    ),
-                    SizedBox(
-                      height: 12,
-                    ),
-                    Text(
-                      'Butuh - Tengaran - Kab. Semarang',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        fontSize: 14
-                      ),
-                    ),
-                  ],
-                ),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Container(
-                      height: 64,
-                      width: 64,
-                      decoration: const BoxDecoration(
-                          shape: BoxShape.circle,
-                          color: Colors.teal
-                      ),
-                      child: IconButton(
-                        onPressed: () {},
-                        icon: const Icon(
-                          Icons.play_arrow_rounded,
-                          // Icons.pause_rounded,
-                          color: Colors.white,
-                          size: 36,
-                        ),
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(12),
+                      child: Image.asset(
+                        "images/album_art.jpg",
+                        fit: BoxFit.cover,
+                        height: 300,
+                        width: 300,
                       ),
                     )
                   ],
                 ),
-                const SizedBox(
-                  height: 24,
+
+
+                Expanded(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Column(
+                        children: const [
+                          Text(
+                            "Radio Da'i",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                                fontSize: 32,
+                                fontWeight: FontWeight.bold
+                            ),
+                          ),
+                          SizedBox(
+                            height: 12,
+                          ),
+                          Text(
+                            'Butuh - Tengaran - Kab. Semarang',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                                fontSize: 14
+                            ),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Container(
+                            height: 64,
+                            width: 64,
+                            decoration: const BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: Colors.teal
+                            ),
+                            child: IconButton(
+                              onPressed: () {},
+                              icon: const Icon(
+                                Icons.play_arrow_rounded,
+                                // Icons.pause_rounded,
+                                color: Colors.white,
+                                size: 36,
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 24,
+                      )
+                    ],
+                  ),
                 )
               ],
             ),
